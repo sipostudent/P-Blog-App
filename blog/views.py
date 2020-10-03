274a80566743ea7bdaff23ac515d55a7434f0e67
Sipo Charles <sipocharles@yahoo.co.uk>
2020-10-03 06:37:07 +0100

    Forms

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
-# blog/views.py
 from django.views.generic import ListView, DetailView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView # new
+from django.urls import reverse_lazy # new
 
 from .models import Post
 
@@ -12,3 +13,21 @@ class BlogListView(ListView):
 class BlogDetailView(DetailView):
     model = Post
     template_name = 'post_detail.html'
+
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title', 'author', 'body']
+
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title', 'body']
+
+
+class BlogDeleteView(DeleteView): # new
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
